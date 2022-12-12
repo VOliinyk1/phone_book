5339a61444f7fadd01f9051c66dbf58e028137b9
VOliinyk1 <vov.25.98@gmail.com>
2022-12-12 12:55:28 +0200

    upd

--- adress_book.py
+++ adress_book.py
@@ -1,51 +1,102 @@
 from collections import UserDict
-from datetime import date, timedelta      
+from datetime import date, timedelta
 import datetime
+import pickle
+
+
 class AddressBook(UserDict):
-    # def initialize_saved_records(self):
-    #     pass
-    
-    # def save_records(self):
-    #     for name, record in self.data.items():
 
+    def save_records(self):
+        formated_recs = {}
+        data_values = tuple(self.data.items())
+        for item in data_values:
+            formated_recs.update({item[0]: {'name': item[1].name.value,
+                                            'phones': item[1].phones,
+                                            'bday': str(item[1].bday)}})
+        save_file = 'records.bin'
 
-    #     # data = {'names' : list(self.data.keys()),
-    #     #         'records' : }
+        with open(save_file, 'wb') as fh:
+            pickle.dump(formated_recs, fh)
+
+        return f'{len(self.data)} records is saved'
+
+    def initialize_saved_records(self):
+        file = 'records.bin'
+        with open(file, 'rb') as fh:
+            init_data = pickle.load(fh)
+        name = Name()
+        bday = Birthday()
+        phones = []
+        for i in init_data.values():
+            name = i['name']
+            phones = [Phone(phone_num) for phone_num in i['phones']]
+            bday = i['bday']
+            new_record = Record(name, phones=phones, birthday=bday)
+            self.add_record(new_record)
+        return (f'{self.data} records is loaded')
 
     def iterator(self, n=1):
         page = []
         for i in range(len(list(self.data.items()))):
-                page.append(list(self.data.items())[i])
-                if list(self.data.items())[i] == list(self.data.items())[-1]:
-                    yield page
-                    page = []
-                if len(page) == n :
-                    yield page
-                    page = []             
-                  
+            page.append(list(self.data.items())[i])
+            if list(self.data.items())[i] == list(self.data.items())[-1]:
+                yield page
+                page = []
+            if len(page) == n:
+                yield page
+                page = []
+
     def search_contact(self, name, phone):
-            if name.isdigit():
-                phone = name
-            if phone:
-                for contact, record in self.items():
-                    if phone in record.get_record_phones():
-                        return contact, record.phones
-            elif name:
-                for contact, record in self.items():
-                    if name == record.name.value:
-                        return contact, record.phones
-            else: f'Contact not founded'
-                
+        if name.isdigit():
+            phone = name
+        if phone:
+            for contact, record in self.items():
+                if phone in record.get_record_phones():
+                    return contact, record.phones
+        elif name:
+            for contact, record in self.items():
+                if name == record.name.value:
+                    return contact, record.phones
+        else:
+            f'Contact not founded'
+
+    def search_matches(self, name, phone):
+        matches = {}
+        if name.isdigit():
+            phone = name
+            name = ''
+
+        if phone:
+            for contact, record in self.items():
+                for rec_phone in record.get_record_phones():
+                    if str(phone) in str(rec_phone):
+                        matches.update({contact: record.get_record_phones()})
+            return matches if matches else 'Matches not founded'
+
+        if name:
+            for contact, record in self.items():
+                if name in record.name.value:
+                    if name not in matches.keys():
+                        matches.update({contact: record})
+            return matches if matches else 'Matches not found'
+
+        else:
+            return 'Matches not found'
+
     def add_record(self, record):
         if record.get_record_phones():
-            self.data[record.name.value] = record
-            return f"Record {record.name.value} {record.get_record_phones()} is added"
-        else: return "Record is not added"
+            self.data.update({record.name.value: record})
+            self.save_records()
+            return (
+                f"Record {record.name} {record.get_record_phones()} is added")
+        else:
+            return "Record is not added"
+
 
 class Field:
     def __init__(self, field_value=None):
         self._value = field_value
-    
+
     @property
     def value(self):
         return self._value
@@ -57,9 +108,7 @@ class Field:
 
 class Name(Field):
     def __repr__(self):
-        return str(self.value)
-    
-    
+        return f'name_field: {str(self.value)}'
 
     @Field.value.setter
     def value(self, new_value):
@@ -68,18 +117,20 @@ class Name(Field):
 
 class Phone(Field):
     def __repr__(self):
-        return str(self.value)
+        return f'{str(self.value)}'
 
     @Field.value.setter
     def value(self, new_value):
-        self._value = new_value if 10 <= len(new_value) <= 12 else None
-
+        self._value = new_value if 10 <= len(str(new_value)) <= 12 else None
 
 
 class Birthday(Field):
     def __repr__(self):
-        return str(self.value)
-    
+        return f'bday_field: {str(self.value)}'
+
+    def __str__(self):
+        return (str(self.value)).replace('-', '.')
+
     @property
     def value(self):
         return super().value
@@ -87,62 +138,65 @@ class Birthday(Field):
     @Field.value.setter
     def value(self, new_value):
         if new_value:
-            date_elems = len(new_value)
+            date_elems = len(str(new_value))
             if date_elems == 10:
-                bday = new_value.split('.')
+                bday = str(new_value).split('.')
                 birthday = date(int(bday[0]), int(bday[1]), int(bday[2]))
                 self._value = birthday
-                
+
             else:
                 print('Wrong date format')
                 self._value = None
 
-        
-
 
 class Record:
-    def __init__(self, name, phone=None, birthday=None):
+    def __init__(self, name, phone=None, phones=[], birthday=None):
         self.name = Name(name)
         self.phone = Phone()
         self.phone.value = phone
-        self.phones = [self.phone.value] if self.phone.value else []
-        self.bday = Birthday() 
+        self.phones = [self.phone.value] if self.phone.value else phones
+        self.bday = Birthday()
         self.bday.value = birthday
 
     def days_to_birthday(self):
         try:
             bday_date = self.bday.value
             now_date = datetime.datetime.now().date()
-            this_year_bday = date(now_date.year,  bday_date.month ,bday_date.day)
+            this_year_bday = date(
+                now_date.year,  bday_date.month, bday_date.day)
             if this_year_bday < now_date:
-                next_bday = date(this_year_bday.year + 1, this_year_bday.month, this_year_bday.day)
+                next_bday = date(this_year_bday.year + 1,
+                                 this_year_bday.month, this_year_bday.day)
                 return f'To bday: {(next_bday - now_date).days} days'
-            else: return f"To bday: {(this_year_bday - now_date).days} days"
+            else:
+                return f"To bday: {(this_year_bday - now_date).days} days"
         except:
             return 'No bday in contact'
 
     def get_record_phones(self):
         return [i for i in self.phones]
-    
+
     def __repr__(self) -> str:
         return str(self.get_record_phones()) + str(self.bday)
-    
+
     def add_bday(self, bday_date):
         self.bday = Birthday()
         self.bday.value = bday_date
-        
+        AddressBook.save_records()
         return f'Bday added' if self.bday.value else f'Bday not added'
 
     def add_phone(self, phone=''):
         if phone:
             self.phones.append(Phone(phone))
+            AddressBook.save_records()
             return f'{self.name} phone {self.get_record_phones()} is added'
         return "Enter phone nuber for adding"
-    
-    def change_phone(self,old_phone, new_phone):
+
+    def change_phone(self, old_phone, new_phone):
         for phone in range(len(self.phones)):
             if self.phones[phone] == str(old_phone):
                 self.phones[phone] = new_phone
+                AddressBook.save_records()
                 return f"{self.name.value}'s number is changed to {new_phone}"
         return 'No such number'
 
@@ -150,29 +204,30 @@ class Record:
         for number in self.phones:
             if number == phone:
                 self.phones.remove(number)
+                AddressBook.save_records()
                 return f"{phone} is removed from {self.name.value}"
             return 'No such phone number'
 
 
+def generate_n_names(n):
+    return [str(i) for i in list(range(n))]
 
-# def generate_n_names(n):
-#     return [str(i) for i in list(range(n))]
 
 # def generate_n_numbers(n):
 #     return [i for i in list(range(n))]
 
-# names = generate_n_names(10)
-# numbers = generate_n_numbers(10)
+
+# names = generate_n_names(2)
+# numbers = generate_n_numbers(2)
 
 
 # book = AddressBook()
 # for name, number in zip(names, numbers):
-#     name = Name(name)
-#     phone = Phone(number)
-#     new_record = Record(name, phone)
+
+#     new_record = Record(name, str(number)*10, birthday='1990.01.12')
 #     book.add_record(new_record)
 
-# for i in book.iterator(3):
-#     print('_______________________________')
-#     for key, value in dict(i).items():
-#         print(f"name {key} record{value}")
+# book.save_records()
+# book = AddressBook()
+# book.initialize_saved_records()
+# print('________________')
--- assist_bot.py
+++ assist_bot.py
@@ -2,11 +2,13 @@ from adress_book import AddressBook, Record
 
 CONTACTS = AddressBook()
 
+
 def command_parser(command_string: str):
     command_elements = command_string.lower().split(' ')
     command = command_elements[0]
     args = ' '.join(i for i in command_elements[1:])
     return command, args
+
 
 def split_args(args_string):
     sep_args = args_string.split(' ')
@@ -16,9 +18,9 @@ def split_args(args_string):
         bday = ''.join(sep_args[2:])
         return name, phone, bday
     else:
-        name = sep_args[0] 
+        name = sep_args[0]
         return name
-    
+
 
 def input_error(func):
     """Handle user's input"""
@@ -35,6 +37,7 @@ def input_error(func):
             return 'Wrong type'
     return wrap
 
+
 def do_command(input):
     new_input = input
     args = ''
@@ -43,31 +46,36 @@ def do_command(input):
             new_input = key
             break
     args = input[len(new_input):].strip()
-        
-    result = COMMANDS.get(new_input, lambda : 'enter one of available commands' )
+
+    result = COMMANDS.get(new_input, lambda: 'enter one of available commands')
     return result(args) if args else result()
+
 
 @input_error
 def hello_handler():
     """Print hello!"""
     return 'hello!'
 
+
 @input_error
 def add_phone(args):
     """Add new contact"""
-    name, phone,_ = split_args(args)
+    name, phone, _ = split_args(args)
     if name:
         return CONTACTS[name].add_phone(phone)
-    else: return f'Enter name'
+    else:
+        return f'Enter name'
+
 
 @input_error
 def change_number(args):
     """Change number"""
-    old_phone, new_phone,_  = split_args(args)
+    old_phone, new_phone, _ = split_args(args)
     for _, record in CONTACTS.items():
         if old_phone in record.get_record_phones():
             return record.change_phone(old_phone, new_phone)
     return 'no such phone number'
+
 
 @input_error
 def remove_phone(args):
@@ -76,17 +84,18 @@ def remove_phone(args):
     for record in CONTACTS.values():
         if phone in record.get_record_phones():
             return record.remove_phone(phone)
-    
 
 
 @input_error
 def add_new_record(args):
     """add new record"""
-    name, phone,_ = split_args(args)
+    name, phone, _ = split_args(args)
     if name not in CONTACTS.keys():
         record = Record(name=name, phone=phone)
         return CONTACTS.add_record(record)
-    else: return(f'record {name} is already exists')
+    else:
+        return (f'record {name} is already exists')
+
 
 @input_error
 def search_records(name, phone=''):
@@ -94,22 +103,22 @@ def search_records(name, phone=''):
     return f'{finded_name} : {finded_phone}'
 
 
-
 @input_error
 def show_all_handler():
     """shows all contacts"""
     return CONTACTS
+
 
 @input_error
 def good_bye_handler():
     """Print Bye"""
     return "Bye!"
 
+
 @input_error
 def iter_records(args):
     page_size = split_args(args)
-    
-    
+
     for i in CONTACTS.iterator(int(page_size)):
         print('_______________________________')
         for key, value in dict(i).items():
@@ -117,10 +126,12 @@ def iter_records(args):
             continue
     return '___________END____________'
 
+
 @input_error
 def add_bday(args):
-    name,bday_date, _ = split_args(args)
+    name, bday_date, _ = split_args(args)
     return CONTACTS[name].add_bday(bday_date) if name in CONTACTS else 'No such name'
+
 
 @input_error
 def days_to_bday(args):
@@ -128,23 +139,44 @@ def days_to_bday(args):
     return CONTACTS[name].days_to_birthday()
 
 
+@input_error
+def save_records():
+    result = CONTACTS.save_records()
+    return result
 
-COMMANDS = {'bday_count' : days_to_bday,
-            'add' : add_new_record,
+
+@input_error
+def load_records():
+    result = CONTACTS.initialize_saved_records()
+    return result
+
+
+@input_error
+def matching_records(name='', phone=''):
+    result = CONTACTS.search_matches(name, phone)
+    return result
+
+
+COMMANDS = {'match': matching_records,
+            'load_records': load_records,
+            'save_records': save_records,
+            'bday_count': days_to_bday,
+            'add': add_new_record,
             'bday': add_bday,
-            'n_records' : iter_records,
+            'n_records': iter_records,
             'hello': hello_handler,
             'show all': show_all_handler,
             'exit': good_bye_handler,
-            'new_record' : add_new_record,
+            'new_record': add_new_record,
             'new_phone': add_phone,
             'change_phone': change_number,
-            'remove' : remove_phone,
-            'add' : add_new_record,
-            'search' : search_records,
-            
-            
+            'remove': remove_phone,
+            'add': add_new_record,
+            'search': search_records,
+
+
             }
+
 
 def main():
     print('''COMMANDS:
@@ -165,7 +197,8 @@ def main():
         result = do_command(user_input)
         print(result)
         if result == 'Bye!':
-            break 
+            break
+
 
 if __name__ == '__main__':
     main()
